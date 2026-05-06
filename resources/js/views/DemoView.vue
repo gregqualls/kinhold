85c95a0eb5e7df113b3d97032be4c4833b5ac0e9
Greg Qualls <84520709+gregqualls@users.noreply.github.com>
2026-05-06 16:50:06 +0100

    feat: Ellis family demo refresh (#243) (#285)
    * chore: capitalise Demo label
    
    * fix: update DemoModal to Ellis family (#243)

--- resources/js/views/DemoView.vue
+++ resources/js/views/DemoView.vue
@@ -7,7 +7,7 @@
           <img src="/images/logo-100.png" alt="Kinhold" class="w-16 h-16 rounded-2xl" />
           <h1 class="text-4xl font-heading font-bold text-kin-gold">Kinhold</h1>
         </router-link>
-        <p class="kin-muted mt-2">Try the demo</p>
+        <p class="kin-muted mt-2">Try the Demo</p>
       </div>
 
       <KinFlatCard padding="lg">
